ab40b2275edf999a145b62d2da8df48e558d7495
Aleksandr <123251018+AleksandrChudinov@users.noreply.github.com>
2024-11-12 00:20:36 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+const btnLoadImages = document.getElementById("load-images-button");
+const gallery = document.getElementById("gallery");
+const loader = document.getElementById("loader");
+const URL = "https://api.thecatapi.com/v/images/search?limit=10";
+
+async function getUrlsArray() {
+  try {
+    const response = await fetch(URL);
+    const data = await response.json();
+    return data.map((object) => object.url);
+  } catch (error) {
+    alert("Что-то сломалось");
+    return [];
+  }
+}
+async function loadImages() {
+  gallery.style.filter = "blur(10px)";
+  loader.style.display = "block";
+  const urlsArray = await getUrlsArray();
+  urlsArray.forEach((url) => {
+    const img = document.createElement("img");
+    gallery.appendChild(img);
+    img.src = url;
+    img.addEventListener("click", () => {
+      window.open(img.src, "_blank");
+    });
+  });
+  loader.style.display = "none";
+  gallery.style.filter = "none";
+  gallery.style.display = "flex";
+}
+btnLoadImages.addEventListener("click", loadImages);
